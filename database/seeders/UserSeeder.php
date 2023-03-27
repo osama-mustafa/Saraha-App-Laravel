f75d94794ca144443948e69ee0e7290807a8a7b1
{Osama Mustafa} <{semsem6661@gmail.com}>
2023-03-27 13:39:29 +0200

    create first user in the system

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@saraha.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => true
+
+        ]);
         User::factory()->count(100)->create();
     }
 }
