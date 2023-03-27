422a50112d46e88cc9b7373e85948518c024c73d
Osama Mustafa <semsem6661@gmail.com>
2023-03-27 13:52:52 +0200

    fix homepage theme

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-success">
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
                 <a class="navbar-brand text-white" href="/"><i class="fa fa-envelope"></i> Saraha App</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
