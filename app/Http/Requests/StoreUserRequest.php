dac9b7f5710992dd30ca25e31890861857ea3576
osama-mustafa <semsem6661@gmail.com>
2023-04-11 09:24:30 +0200

    Create StoreUserRequest

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name'      => ['required', 'max:255', 'unique:users,name'],
+            'email'     => ['required', 'unique:users,email'],
+            'image'     => ['required', 'mimes:jpeg,jpg,png,gif|max:2048'],
+            'password'  => ['required']
+        ];
+    }
+}
