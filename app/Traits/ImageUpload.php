fcfee6c7bc2312a4c3eca20600912213ee3bb8fc
osama-mustafa <semsem6661@gmail.com>
2023-04-15 00:01:26 +0200

    Fix ImageUpload Trait

--- app/Traits/ImageUpload.php
+++ app/Traits/ImageUpload.php
@@ -7,24 +7,19 @@ use Illuminate\Support\Facades\Storage;
 
 trait ImageUpload {
 
-    function handleUploadImage(Request $request, $inputName = 'image', $path = 'public/images') {
-        try {
-            
-            // Validate image
-            $request->validate([
-                $inputName => ['required', 'image', 'mimes:jpeg,png,jpg,gif|max:2048'],
-            ]);
+    function handleUploadImage(Request $request, $inputName = 'image', $path = 'public/images') {     
+        
+        // Validate image
+        $request->validate([
+            $inputName => ['required', 'image', 'mimes:jpeg,png,jpg,gif|max:2048'],
+        ]);
 
-            // Create unique name for the image
-            $file  = $request->$inputName;
-            $name  =  $file->hashName();
+        // Create unique name for the image
+        $file  = $request->$inputName;
+        $name  =  $file->hashName();
 
-            // Save image to specific directory
-            $path = Storage::putFileAs($path, $file, $name);
-            return $name;
-
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
+        // Save image to specific directory
+        $path = Storage::putFileAs($path, $file, $name);
+        return $name;
     }
 }
